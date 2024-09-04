0f24ad769badf24dbfdb0e8820b7c1e57520254b
PRISILA BERENICE LUNA DELGADO <prisila.lunad@uanl.edu.mx>
2024-09-03 22:13:55 -0600

    main4

---
+++ AAVD/DEPARTAMENTOS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AAVD
+{
+    public partial class DEPARTAMENTOS : Form
+    {
+        public DEPARTAMENTOS()
+        {
+            InitializeComponent();
+        }
+    }
+}
